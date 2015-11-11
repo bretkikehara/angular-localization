e1a74526e3ff0846fca7e8ff04983c075240daa6
bret.ikehara <bret.ikehara@dailymotion.com>
2015-11-10 23:48:24 -0800

    chore(demo): Initial investigation into issue 76
    Initial investigation into issue 76 of basePath.

--- app/src/core/routes.js
+++ app/src/core/routes.js
@@ -13,6 +13,16 @@ angular.module('myApp.core.router')
                 });
         }
     ])
+    .value('localeConf', {
+        basePath: './localization/languages',
+        defaultLocale: 'en-US',
+        sharedDictionary: 'common',
+        fileExtension: '.lang.json',
+        persistSelection: true,
+        cookieName: 'COOKIE_LOCALE_LANG',
+        observableAttrs: new RegExp('^data-(?!ng-|i18n)'),
+        delimiter: '::'
+    })
     .value('localeSupported', [
         'en-US',
         'fr-FR'
